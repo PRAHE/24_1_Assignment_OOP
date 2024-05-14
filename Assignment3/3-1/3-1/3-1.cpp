5cadbeab4db047893645e6301396044da88b09e8
PRAHE <wendydeer1020@gmail.com>
2024-05-14 16:16:13 +0900

    Commit; 3-1 finished

--- Assignment3/3-1/3-1/3-1.cpp
+++ Assignment3/3-1/3-1/3-1.cpp
@@ -10,35 +10,33 @@ private:
 	Node* pNext;
 
 public:
-	Node() : value(), pNext(nullptr) {};
 	void setValue(string value) { this->value = value; }
 	void setNext(Node* pNext) { this->pNext = pNext; }
-	string getValue() { return value; }
-	Node* getNext() { return pNext; }
+	string getValue() { return this->value; }
+	Node* getNext() { return this->pNext; }
 };
 
 class LinkedList {
 public:
+	LinkedList() { Node node; pHead->setNext(NULL); pHead->setValue("\0"); }
 	bool initalize(string str, int cur_pos);
-	void transfer(string str, int cur_pos);
+	bool transfer(string str, int cur_pos);
 	void print();
 
 private:
 	Node* pHead = new Node;
-	Node* pTail = new Node;
 };
 
 bool LinkedList::initalize(string str, int cur_pos) {
 	int pos;
 	string result;
 
-	while ((pos = str.find(' ', cur_pos)) != string::npos) {
-		int len = pos - cur_pos;
+	while ((pos = str.find(' ', cur_pos)) != string::npos || cur_pos == str.size() - 2) {
+		int len = pos - cur_pos; //40으로 결과값 나올 때 pos가 -1로 됨. 확인필요
 		result = str.substr(cur_pos, len);
-
-		if (pHead->getNext() == NULL) {
+		if (pHead->getValue() == "\0") {
 			pHead->setValue(result);
-			pHead->setNext(pTail);
+			pHead->setNext(nullptr);
 		}
 
 		else {
@@ -49,40 +47,81 @@ bool LinkedList::initalize(string str, int cur_pos) {
 			pNew->setValue(result); //노드 끝에 값 추가 수정
 
 			Node* curNode = pHead;
-			while (curNode->getNext() != pTail) { //연결리스트의 끝 찾기
+			while (curNode->getNext() != nullptr) { //연결리스트의 끝 찾기
 				curNode = curNode->getNext();
 			}
-			curNode->setNext(pNew); //가장 끝노드가 pNew를 가리키도록
-
+			curNode->setNext(pNew); //마지막 노드가 pNew를 가리키도록
+			pNew->setNext(nullptr); //pNew는 nullptr을 가리키도록
 		}
 		cur_pos = pos + 1;
+		if (cur_pos == 0) { break; }
 	}
-	pTail->setNext(pHead);
-	pTail->setValue(result);
 
 	Node* curNode = pHead;
-
-	while (curNode->getNext() != pHead) {
-		cout << curNode->getValue() << ' ';
+	while (curNode->getNext() != nullptr) {
 		curNode = curNode->getNext();
 	}
-
+	curNode->setNext(pHead); //pTail과 마지막 노드 이어줌.
 	return true;
 }
 
-void LinkedList::transfer(string str, int cur_pos) {
+bool LinkedList::transfer(string str, int cur_pos) {
 	int pos;
-	int dis = 0;
-
-	dis = rand() % 10;
-
-	while ((pos = str.find(' ', cur_pos)) != string::npos) {
-		int len = pos - cur_pos;
-		string result = str.substr(cur_pos, len);
-
-		cout << result << endl;
-		cur_pos = pos + 1;
+	int k = 0;
+	string result;
+	int transfer[2] = { 0, };
+	//disconnected
+	int num = rand() % 10;
+	cout << "num: " << num << endl;
+	Node* check = pHead;
+	for (int i = 0; i < num; i++) { //num수만큼 노드 건너뛰기
+		check = check->getNext();
 	}
+	cout << check->getValue() << endl;
+	check->setNext(nullptr); //disconnect
+
+	while ((pos = str.find(' ', cur_pos)) != string::npos || cur_pos == str.size() - 1) {
+		int len = pos - cur_pos;
+		result = str.substr(cur_pos, len);
+
+		if (result == "to") {}
+		else { 
+			transfer[k] = stoi(result);
+			k++; 
+		}
+
+		cur_pos = pos + 1;
+		if (cur_pos == 0) { break; }
+	}
+	int sub = transfer[1] - transfer[0];
+	if (sub < 0) {
+		sub += 10;
+	}
+
+	Node* first = pHead;
+	int i;
+	for (i = 0; i < transfer[i]; i++) {
+		first = first->getNext();
+		if (first == nullptr) {
+			cout << "Detected a disconnection between " << i << " and " << i + 1 << endl;
+			return 0;
+		}
+	}
+	string key = first->getValue();
+	for (int j = i; j < i + sub; j++) {
+		first = first->getNext();
+		if (first == nullptr) {
+			if (j >= 10) {
+				j -= 10;
+			}
+			cout << "Detected a disconnection between " << j << " and " << j + 1 << endl;
+			return 0;
+		}
+	}
+	Node* second = first;
+	second->setValue(key);
+
+	return 1;
 }
 
 void LinkedList::print() {
@@ -92,6 +131,7 @@ void LinkedList::print() {
 		cout << curNode->getValue() << ' ';
 		curNode = curNode->getNext();
 	}
+	cout << curNode->getValue() << endl;
 }
 
 int main() {
@@ -99,32 +139,34 @@ int main() {
 	string str;
 	char tmp[20];
 	int pos;
-	int cur_pos = 0;
 
 	srand((unsigned)time(NULL));
 
 	while (1) {
 		cout << "Command : ";
 		getline(cin, str);
+		int cur_pos = 0;
 
-		pos = str.find(' ', cur_pos);
+ 		pos = str.find(' ', cur_pos);
+		if (pos == string::npos) {
+			pos = str.find("\n", cur_pos);
+		}
 		int len = pos - cur_pos;
 		string result = str.substr(cur_pos, len);
-
 		strcpy(tmp, result.c_str());
-		if (strcmp(tmp, "initilalize")) {
+
+		if (!strcmp(tmp, "initialize")) {
 			list.initalize(str, pos + 1);
 		}
-		else if (strcmp(tmp, "transfer")) {
-			list.transfer(str, cur_pos);
+		else if (!strcmp(tmp, "transfer")) {
+			list.transfer(str, pos + 1);
 		}
-		else if (strcmp(tmp, "print")) {
+		else if (!strcmp(tmp, "print")) {
 			list.print();
 		}
-		else if (strcmp(tmp, "exit")) {
+		else if (!strcmp(tmp, "exit")) {
 			break;
 		}
 	}
-
 	return 0;
 }
